5374fed4478f49de4f519582c6ceb02ecf982f35
Shubh <shubhsingh8767@gmail.com>
2023-12-24 23:23:35 +0530

    useState Timeout

--- advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,20 +3,26 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
 
-  // this is not synchronous
   // const handleClick = () => {
-  //   setValue(value + 1);
-  //   console.log(value);
+  //   setTimeout(() => {
+  //     console.log("clicked the button");
+  //     setValue(value + 1);
+  //   }, 3000);
   // };
 
-  // If you want to update state immediately and synchronously, you can pass a function to setState
+  // In the above function if a click button 'n' times within 3 seconds (or whatevere time you set) the value will not increase by 'n' times
+  // for example - initially the value is zero and i clicked the button 5 times with 3 seconds it will only do '0+1' 5 times
+  // As it uses the value right before we invoked the call back function
+
+  // the solution for this is function approach
 
   const handleClick = () => {
-    setValue((currentState) => {
-      const newState = currentState + 1;
-      console.log(newState);
-      return newState;
-    });
+    setTimeout(() => {
+      console.log("clicked the button");
+      setValue((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
   };
 
   return (
