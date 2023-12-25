b84a9e2e78f2d2c4c19c391900f434d2d6bd6976
Shubh <shubhsingh8767@gmail.com>
2023-12-25 16:01:39 +0530

    useEffect challenge

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,9 +1,9 @@
-import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects";
+import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
 
 function App() {
   return (
     <>
-      <MultipleEffects />
+      <FetchData />
     </>
   );
 }
